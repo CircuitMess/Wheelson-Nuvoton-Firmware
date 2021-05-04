88e1a64188f7fd45c5a7df03d371e241675b5505
Domagoj <domagoj.zoraja@fer.hr>
2021-05-04 10:14:12 +0200

    remove uint declarations

--- inc/Typedef.h
+++ inc/Typedef.h
@@ -6,8 +6,4 @@ typedef unsigned char UINT8;
 typedef unsigned int UINT16;
 typedef unsigned long UINT32;
 
-typedef unsigned char uint8_t;
-typedef unsigned int uint16_t;
-typedef unsigned long uint32_t;
-
 #endif
