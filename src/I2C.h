ea76d94a62d3626cfbc238ba0243696c6497aaf9
Domagoj <domagoj.zoraja@fer.hr>
2021-05-19 14:04:38 +0200

    clear unnecessary defines for i2c interface

--- src/I2C.h
+++ src/I2C.h
@@ -19,17 +19,6 @@ struct i2cCommand {
 /*	I2C INTERFACE  */
 #define SLAVE_ADDR 0x38			//  slave address (ESP->master, N76->slave)
 
-#define ID_VERIFICATION 0x00	//  return slave address
-#define RESET_COMMAND 0x01		//  reset uC
-#define SET_BACKLIGHT 0x20		//	receive 1 byte (bool)
-#define GET_BACKLIGHT 0x21		//	return 1 byte (bool)
-#define SET_HEADLIGHT 0x22		//	receive 1 byte (uint8) - intensity
-#define GET_HEADLIGHT 0x23		//	return 1 byte (uint8) - intensity
-#define SET_MOTOR 0x30			//	receive 2 bytes: (uint8) - motor ID & (int8) - intensity & rotation
-#define GET_MOTOR 0x31			//	receive 1 byte (uint8) - motor ID & return 1 byte (int8) - intensity & rotation
-#define NUM_EVENTS 0x40			//	return 1 byte (uint8) - number of events
-#define EVENT_HANDLER 0x41		//	receive 1 byte (uint8) - number of events & return N bytes 1B=>(state+ID)
-
 void i2cInit(const struct i2cCommand* _commands, uint8_t _numCommands);
 void i2cRespond(uint8_t* data, uint8_t count);
 
