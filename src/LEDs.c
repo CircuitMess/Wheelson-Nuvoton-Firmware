390a3e39ab6ff6de6a5b6fe91bf431fc423b7118
Domagoj <61870770+dz0102@users.noreply.github.com>
2021-05-19 13:22:04 +0200

    RGB LED (#3)

--- src/LEDs.c
+++ src/LEDs.c
@@ -3,8 +3,12 @@
 #include "N76E616.h"
 #include "Define.h"
 
+__far uint8_t rgb = 0;
+
 void LEDInit(){
+
 	LED_PIN_INIT();
+	RGB_INIT();
 }
 
 void setBacklight(bool backlight){
@@ -21,4 +25,17 @@ void setHeadlight(uint8_t headlight){
 
 uint8_t getHeadlight(){
 	return !HEAD_LIGHT;
+}
+
+void setRGB(uint8_t _rgb){
+
+	rgb = ~_rgb;
+	rgb &= 0x07;
+
+	P4 |= 0x70;
+	P4 &= (rgb<<4);
+}
+uint32_t getRGB(){
+
+	return rgb;
 }
--- src/LEDsInternal.h
+++ src/LEDsInternal.h
@@ -4,10 +4,20 @@
 /* LED PINS */
 #define TFT_BACKLIGHT	P12
 #define HEAD_LIGHT		P13
+/* RGB LED PINS -> on register P4 which is not bit-addressable */
+
 /* LED PINS GPIO MODE */
 #define P12_PushPull_Mode	P1M1 &= ~SET_BIT2; P1M2 |= SET_BIT2;
 #define P13_PushPull_Mode	P1M1 &= ~SET_BIT3; P1M2 |= SET_BIT3;
 
+/* All the used COM and SEG pins generate analog output waveforms.
+ * Therefore, all the corresponding I/O output mode should be set as input-only (high impedance) mode. */
+#define P44_PushPull_Mode	P4M1 &= ~SET_BIT4; P4M2 |= SET_BIT4;
+#define P45_PushPull_Mode	P4M1 &= ~SET_BIT5; P4M2 |= SET_BIT5;
+#define P46_PushPull_Mode	P4M1 &= ~SET_BIT6; P4M2 |= SET_BIT6;
+
+
 #define LED_PIN_INIT()	do{ P12_PushPull_Mode; P13_PushPull_Mode; }while(0);
+#define RGB_INIT()		do{ P44_PushPull_Mode; P45_PushPull_Mode; P46_PushPull_Mode; P4 &= 0x00; P4 |= 0x70; }while(0);
 
 #endif //WHEELSON_NUVOTON_FIRMWARE_LEDSINTERNAL_H
